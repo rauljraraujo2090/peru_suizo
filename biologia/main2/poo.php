641f6228ca223d0ff3b9880ce9704dc58a374619
Raul Jr <131274781+rauljraraujo2090@users.noreply.github.com>
2024-05-30 08:32:10 -0500

    sasa

---
+++ biologia/main2/poo.php
@@ -0,0 +1,17 @@
+<?php
+
+$nombre="Juan";
+$nombre2="Raul";
+
+function mostrar($nombre2){
+return $nombre2."dice hola";
+
+}
+echo $nombre2."<br>";
+echo mostrar($nombre2);
+
+ class raul{
+
+
+}
+?>
